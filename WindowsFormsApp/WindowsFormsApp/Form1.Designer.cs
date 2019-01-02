848d877427b829d4b970152dcbd4a5a57aab7977
peted.dev <peted.dev@outlook.com>
2019-01-03 01:49:18 +0800

    buttons added for comit test

--- WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 107);
+            this.button1.Location = new System.Drawing.Point(261, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 57);
+            this.button1.Size = new System.Drawing.Size(66, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,20 +45,22 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(234, 52);
+            this.button2.Location = new System.Drawing.Point(156, 34);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 66);
+            this.button2.Size = new System.Drawing.Size(67, 43);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
